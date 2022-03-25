e6cd617529eb905aad2f6bb84649ea127698458e
Youngseop Bae <byseop@gmail.com>
2022-03-25 18:48:38 +0900

    Initial commit

---
+++ src/utils/hooks/useInit.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+export default function useInit() {
+  // 첫 앱 실행시 필요한 항목들을 관리한다.
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: 'seop-bit.firebaseapp.com',
+      projectId: 'seop-bit',
+      storageBucket: 'seop-bit.appspot.com',
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID,
+      measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    getAnalytics(app);
+  }, []);
+}
